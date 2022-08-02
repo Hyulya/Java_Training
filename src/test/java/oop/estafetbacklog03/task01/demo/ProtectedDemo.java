7978bf3b66e8a78746d630dd21a2f95809622b9e
HyuEstafet <hyulya.gyuler@estafet.com>
2022-08-02 17:44:06 +0300

    updates

--- src/test/java/oop/estafetbacklog03/task01/demo/ProtectedDemo.java
+++ src/test/java/oop/estafetbacklog03/task01/demo/ProtectedDemo.java
@@ -9,12 +9,15 @@ protected access modifier.
  */
 
 import oop.estafetbacklog03.task01.classes.printer.MultimediaPrinter;
+import oop.estafetbacklog03.task01.classes.scanner.ScannerDevice;
 
 public class ProtectedDemo {
     public static void main(String[] args) {
         String someText = "random text";
         MultimediaPrinter multimediaPrinter = new MultimediaPrinter(100,true);
         multimediaPrinter.scanAndPrint("some text");
-//        multimediaPrinter.content; // to show that we can't access the field "content" - the ProtectedDemo
+//        multimediaPrinter.content; // to show that we can't access the field "content" from the ProtectedDemo
+        ScannerDevice scannerDevice = new ScannerDevice();
+//        scannerDevice.content // to show that we can't access the field "content" from the ProtectedDemo
     }
 }
